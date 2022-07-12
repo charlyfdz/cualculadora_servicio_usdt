4cd55c42d6d044601867a892c0d7c7c1909a8dc6
charlyfdz <carlos.afdzf@gmail.com>
2022-07-11 19:07:04 -0500

    calculadora para servicio de compra de USDT con html, css y js

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let usdt = document.getElementsByClassName("usdt")[0]
+let cambio = document.getElementsByClassName('cambio')[0]
+let spread = document.getElementsByClassName('spread')[0]
+let comision = document.getElementsByClassName('comision')[0]
+
+let mxn = document.getElementsByClassName('mxn')[0]
+let usd = document.getElementsByClassName('usd')[0]
+
+
+let cambio_precio = 0
+let spread_procentaje = 0.03
+let comision_procentaje = 0.02
+
+cambio.value = cambio_precio
+
+usdt.addEventListener('change',()=>{
+    calcular()
+})
+
+cambio.addEventListener('change',()=>{
+    calcular()
+})
+
+function calcular(){
+
+    axios.get("https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=mxn").then(response =>{
+        cambio_precio = response.data.tether.mxn
+        cambio.value = cambio_precio*(1+.03)
+    })
+    spread.value = (usdt.value * spread_procentaje).toFixed(2)
+    comision.value = (usdt.value * comision_procentaje).toFixed(2)
+
+
+    usd_total = Number(usdt.value) + Number(spread.value) + Number(comision.value)
+    mxn_total = usd_total*Number(cambio.value)
+
+    mxn.textContent = mxn_total.toFixed(2)
+    usd.textContent = usd_total.toFixed(2)
+}
