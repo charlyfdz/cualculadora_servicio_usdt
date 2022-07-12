85816c7b49adde0a6969a7f557f076165eb989c3
charlyfdz <carlos.afdzf@gmail.com>
2022-07-11 19:31:45 -0500

    fixed mxn result

--- index.js
+++ index.js
@@ -12,29 +12,17 @@ let cambio_precio = 0
 let spread_procentaje = 0.03
 let comision_procentaje = 0.02
 
-cambio.value = cambio_precio
-
 usdt.addEventListener('change',()=>{
-    calcular()
-})
-
-cambio.addEventListener('change',()=>{
-    calcular()
-})
-
-function calcular(){
-
     axios.get("https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=mxn").then(response =>{
         cambio_precio = response.data.tether.mxn
-        cambio.value = cambio_precio*(1+.03)
+        cambio.value = cambio_precio*(1+.05)
+        spread.value = (usdt.value * spread_procentaje).toFixed(2)
+        comision.value = (usdt.value * comision_procentaje).toFixed(2)
+        usd_total = Number(usdt.value) + Number(spread.value) + Number(comision.value)
+        mxn_total = (Number(usdt.value) + Number(spread.value) + Number(comision.value))*Number(cambio.value)
+
+        mxn.textContent = mxn_total.toFixed(2)
+        usd.textContent = usd_total.toFixed(2)
     })
-    spread.value = (usdt.value * spread_procentaje).toFixed(2)
-    comision.value = (usdt.value * comision_procentaje).toFixed(2)
+})
 
-
-    usd_total = Number(usdt.value) + Number(spread.value) + Number(comision.value)
-    mxn_total = usd_total*Number(cambio.value)
-
-    mxn.textContent = mxn_total.toFixed(2)
-    usd.textContent = usd_total.toFixed(2)
-}
